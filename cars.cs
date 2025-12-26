7dffd2dcbf915bbeaae945de50f6d2640707fbdf
Youssefsalemeh <92230536@students.liu.edu.lb>
2025-12-26 13:42:05 +0200

    Add files via upload
    when we click button open new form differentes

---
+++ cars.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class cars : Form
+    {
+        public cars()
+        {
+            InitializeComponent();
+        }
+
+        private void cars_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login main = new Login();
+            main.Show();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            addcars main = new addcars();
+            main.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Customer main = new Customer();
+            main.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CarRental main = new CarRental();
+            main.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            users main = new users();
+            main.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
