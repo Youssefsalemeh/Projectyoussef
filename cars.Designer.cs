c9ba71f774bc83354c0b33df801eff685767782f
Youssefsalemeh <92230536@students.liu.edu.lb>
2025-12-26 13:38:53 +0200

    Add files via upload
    this page have 5 button in the left

---
+++ cars.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace FinalProject
+{
+    partial class cars
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cars));
+            panel1 = new Panel();
+            panel6 = new Panel();
+            button4 = new Button();
+            pictureBox5 = new PictureBox();
+            panel5 = new Panel();
+            button3 = new Button();
+            pictureBox4 = new PictureBox();
+            panel4 = new Panel();
+            button2 = new Button();
+            pictureBox3 = new PictureBox();
+            panel3 = new Panel();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            button5 = new Button();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            pictureBox6 = new PictureBox();
+            button6 = new Button();
+            panel1.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Controls.Add(panel6);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(219, 458);
+            panel1.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Black;
+            panel6.Controls.Add(button4);
+            panel6.Controls.Add(pictureBox5);
+            panel6.Location = new Point(3, 328);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(210, 65);
+            panel6.TabIndex = 3;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Black;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(68, 10);
+            button4.Name = "button4";
+            button4.Size = new Size(136, 43);
+            button4.TabIndex = 0;
+            button4.Text = "User";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(3, 10);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(46, 43);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Black;
+            panel5.Controls.Add(button3);
+            panel5.Controls.Add(pictureBox4);
+            panel5.Location = new Point(9, 250);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(210, 53);
+            panel5.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Black;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(62, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(136, 50);
+            button3.TabIndex = 0;
+            button3.Text = "Rental";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(-3, 3);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(46, 43);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 8;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Black;
+            panel4.Controls.Add(button2);
+            panel4.Controls.Add(pictureBox3);
+            panel4.Location = new Point(6, 176);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(210, 53);
+            panel4.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(55, 7);
+            button2.Name = "button2";
+            button2.Size = new Size(146, 49);
+            button2.TabIndex = 0;
+            button2.Text = "Custumer";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(3, 7);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(46, 43);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Black;
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(3, 100);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(213, 53);
+            panel3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(68, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 53);
+            button1.TabIndex = 0;
+            button1.Text = "Car";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(46, 53);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(213, 91);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Controls.Add(button5);
+            panel2.Location = new Point(3, 398);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(216, 57);
+            panel2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Black;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(-3, 1);
+            button5.Name = "button5";
+            button5.Size = new Size(219, 53);
+            button5.TabIndex = 0;
+            button5.Text = "Logout";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 15;
+            guna2Elipse1.TargetControl = this;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(219, 0);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(640, 640);
+            pictureBox6.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Transparent;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.ForeColor = Color.Red;
+            button6.Location = new Point(807, 0);
+            button6.Name = "button6";
+            button6.Size = new Size(34, 38);
+            button6.TabIndex = 12;
+            button6.Text = "X";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // cars
+            // 
+            AutoScaleDimensions = new SizeF(13F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(840, 458);
+            Controls.Add(button6);
+            Controls.Add(pictureBox6);
+            Controls.Add(panel1);
+            Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "cars";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "cars";
+            Load += cars_Load;
+            panel1.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Panel panel3;
+        private PictureBox pictureBox1;
+        private Panel panel4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private Panel panel5;
+        private PictureBox pictureBox4;
+        private Button button1;
+        private Panel panel6;
+        private Button button4;
+        private PictureBox pictureBox5;
+        private Button button3;
+        private Button button2;
+        private Button button5;
+        private PictureBox pictureBox6;
+        private Button button6;
+    }
+}
